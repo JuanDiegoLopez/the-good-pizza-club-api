3a325360b3e83f9e694e6c23f3d1487ee696ced3
Juan Diego Lopez <juandlopez@msn.com>
2023-02-24 10:26:21 -0500

    feat(setup): setup typeorm and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const configureTypeORM = (configService: ConfigService) => {
+  return {
+    type: 'sqlite',
+    database: configService.get('DB_NAME'),
+    entities: [],
+    // TODO: set to false before going to production
+    synchronize: true,
+  } as TypeOrmModuleOptions;
+};
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: configureTypeORM,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
